86cea849c4edf2131078f79fd61f4850809d6dd1
BenKelenski <ben.kelenski@gmail.com>
2025-08-07 19:31:29 -0400

    remove unneeded annotation

--- src/main/kotlin/dev/benkelenski/booked/domain/Book.kt
+++ src/main/kotlin/dev/benkelenski/booked/domain/Book.kt
@@ -1,15 +1,14 @@
 package dev.benkelenski.booked.domain
 
-import com.squareup.moshi.Json
 import java.time.Instant
 
 data class Book(
     val id: Int,
-    @Json(name = "google_id") val googleId: String,
+    val googleId: String,
     val title: String,
     val authors: List<String>,
-    @Json(name = "thumbnail_url") val thumbnailUrl: String?,
-    @Json(name = "created_at") val createdAt: Instant,
-    @Json(name = "user_id") val userId: Int,
-    @Json(name = "shelf_id") val shelfId: Int,
+    val thumbnailUrl: String?,
+    val createdAt: Instant,
+    val userId: Int,
+    val shelfId: Int,
 )
--- src/main/kotlin/dev/benkelenski/booked/domain/Shelf.kt
+++ src/main/kotlin/dev/benkelenski/booked/domain/Shelf.kt
@@ -1,12 +1,11 @@
 package dev.benkelenski.booked.domain
 
-import com.squareup.moshi.Json
 import java.time.Instant
 
 data class Shelf(
     val id: Int,
-    @Json(name = "user_id") val userId: Int,
+    val userId: Int,
     val name: String,
     val description: String?,
-    @Json(name = "created_at") val createdAt: Instant,
+    val createdAt: Instant,
 )
