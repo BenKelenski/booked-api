cf905d57d625a8a55cf7ed3e84bdd43f92c8026c
BenKelenski <ben.kelenski@gmail.com>
2025-08-07 19:31:56 -0400

    add IDs to book and shelf responses

--- src/main/kotlin/dev/benkelenski/booked/domain/responses/BookResponse.kt
+++ src/main/kotlin/dev/benkelenski/booked/domain/responses/BookResponse.kt
@@ -4,15 +4,17 @@ import com.squareup.moshi.Json
 import dev.benkelenski.booked.domain.Book
 
 data class BookResponse(
-    val googleId: String,
+    val id: Int,
+    @param:Json(name = "google_id") val googleId: String,
     val title: String,
     val authors: List<String>,
-    @Json(name = "thumbnail_url") val thumbnailUrl: String? = null,
-    @Json(name = "created_at") val createdAt: String,
+    @param:Json(name = "thumbnail_url") val thumbnailUrl: String? = null,
+    @param:Json(name = "created_at") val createdAt: String,
 ) {
     companion object {
         fun from(book: Book): BookResponse =
             BookResponse(
+                id = book.id,
                 googleId = book.googleId,
                 title = book.title,
                 authors = book.authors,
--- src/main/kotlin/dev/benkelenski/booked/domain/responses/ShelfResponse.kt
+++ src/main/kotlin/dev/benkelenski/booked/domain/responses/ShelfResponse.kt
@@ -4,13 +4,15 @@ import com.squareup.moshi.Json
 import dev.benkelenski.booked.domain.Shelf
 
 data class ShelfResponse(
+    val id: Int,
     val name: String,
     val description: String? = null,
-    @Json(name = "created_at") val createdAt: String,
+    @param:Json(name = "created_at") val createdAt: String,
 ) {
     companion object {
         fun from(shelf: Shelf): ShelfResponse =
             ShelfResponse(
+                id = shelf.id,
                 name = shelf.name,
                 description = shelf.description,
                 createdAt = shelf.createdAt.toString(),
