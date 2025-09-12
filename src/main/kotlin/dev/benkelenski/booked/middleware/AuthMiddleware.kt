3bed123bbcd838ac519a34e9bff428d6f169333b
BenKelenski <ben.kelenski@gmail.com>
2025-09-12 19:48:17 -0400

    AuthMiddleware returns ApiError

--- src/main/kotlin/dev/benkelenski/booked/middleware/AuthMiddleware.kt
+++ src/main/kotlin/dev/benkelenski/booked/middleware/AuthMiddleware.kt
@@ -1,10 +1,11 @@
 package dev.benkelenski.booked.middleware
 
 import dev.benkelenski.booked.auth.TokenProvider
-import org.http4k.core.Filter
-import org.http4k.core.Request
-import org.http4k.core.Response
-import org.http4k.core.Status
+import dev.benkelenski.booked.constants.ErrorCodes
+import dev.benkelenski.booked.constants.ErrorTypes
+import dev.benkelenski.booked.domain.responses.ApiError
+import dev.benkelenski.booked.http.apiErrorLens
+import org.http4k.core.*
 import org.http4k.core.cookie.cookie
 
 /** alias for [authMiddleware] */
@@ -14,11 +15,27 @@ fun authMiddleware(tokenProvider: TokenProvider): Filter = Filter { next ->
     { req: Request ->
         val token =
             req.cookie("access_token")?.value
-                ?: return@Filter Response(Status.UNAUTHORIZED).body("Missing access token")
+                ?: return@Filter Response(Status.UNAUTHORIZED)
+                    .with(
+                        Body.apiErrorLens of
+                            ApiError(
+                                message = "Missing access token",
+                                code = ErrorCodes.MISSING_ACCESS_TOKEN,
+                                type = ErrorTypes.AUTHENTICATION,
+                            )
+                    )
 
         val userId =
             tokenProvider.extractUserId(token)
-                ?: return@Filter Response(Status.UNAUTHORIZED).body("Invalid or expired token")
+                ?: return@Filter Response(Status.UNAUTHORIZED)
+                    .with(
+                        Body.apiErrorLens of
+                            ApiError(
+                                message = "Invalid token",
+                                code = ErrorCodes.INVALID_ACCESS_TOKEN,
+                                type = ErrorTypes.AUTHENTICATION,
+                            )
+                    )
 
         next(req.header("X-User-Id", userId.toString()))
     }
