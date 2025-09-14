26826cf0b425290df6a7891608c4fa23cca583e3
BenKelenski <ben.kelenski@gmail.com>
2025-09-13 20:20:11 -0400

    create HttpConstants.kt and values

---
+++ src/main/kotlin/dev/benkelenski/booked/constants/HttpConstants.kt
@@ -0,0 +1,8 @@
+package dev.benkelenski.booked.constants
+
+object HttpConstants {
+    const val USER_ID_HEADER = "X-User-Id"
+
+    const val ACCESS_TOKEN_COOKIE = "access_token"
+    const val REFRESH_TOKEN_COOKIE = "refresh_token"
+}
