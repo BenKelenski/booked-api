2da8677f38f9e491cb84dfbfab8ca1b3fadb6a26
BenKelenski <ben.kelenski@gmail.com>
2025-09-13 20:20:21 -0400

    move lenses to own file

--- src/main/kotlin/dev/benkelenski/booked/http/Lenses.kt
+++ src/main/kotlin/dev/benkelenski/booked/http/Lenses.kt
@@ -1,8 +1,30 @@
 package dev.benkelenski.booked.http
 
+import dev.benkelenski.booked.domain.requests.BookRequest
+import dev.benkelenski.booked.domain.requests.ShelfRequest
 import dev.benkelenski.booked.domain.responses.ApiError
+import dev.benkelenski.booked.domain.responses.ShelfResponse
 import org.http4k.core.Body
 import org.http4k.format.Moshi.auto
+import org.http4k.lens.Path
+import org.http4k.lens.int
 
+// Shelf lenses
+val shelfIdLens = Path.int().of("shelf_id")
+
+val Body.Companion.shelfReqLens
+    get() = Body.auto<ShelfRequest>().toLens()
+
+val Body.Companion.shelfResLens
+    get() = Body.auto<ShelfResponse>().toLens()
+
+val Body.Companion.shelvesResLens
+    get() = Body.auto<Array<ShelfResponse>>().toLens()
+
+// Book lenses
+val Body.Companion.bookReqLens
+    get() = Body.auto<BookRequest>().toLens()
+
+// Error lenses
 val Body.Companion.apiErrorLens
     get() = Body.auto<ApiError>().toLens()
