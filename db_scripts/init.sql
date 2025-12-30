a58e442c8013464d18381f75301e203caa18ddb4
BenKelenski <ben.kelenski@gmail.com>
2025-12-30 00:16:45 -0500

    DB sql changes

--- db_scripts/init.sql
+++ db_scripts/init.sql
@@ -5,7 +5,7 @@ CREATE DATABASE booked;
 CREATE TABLE IF NOT EXISTS users
 (
     id         SERIAL PRIMARY KEY,
-    email      VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL UNIQUE,
     name       VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
@@ -26,56 +26,42 @@ CREATE TABLE auth_identities
 CREATE TABLE IF NOT EXISTS refresh_tokens
 (
     id         UUID PRIMARY KEY,
-    user_id    INTEGER   NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    token_hash TEXT      NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    expires_at TIMESTAMP NOT NULL
+    user_id    INTEGER     NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    token_hash TEXT        NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMPTZ NOT NULL
 );
-
+CREATE INDEX idx_refresh_tokens_user_id ON refresh_tokens (user_id);
 
 CREATE TABLE IF NOT EXISTS shelves
 (
-    id          SERIAL PRIMARY KEY,
-    user_id     INTEGER      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    name        VARCHAR(150) NOT NULL,
-    description VARCHAR(250) NULL,
-    created_at  TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP
+    id             SERIAL PRIMARY KEY,
+    user_id        INTEGER      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    name           VARCHAR(150) NOT NULL,
+    description    VARCHAR(250) NULL,
+    is_deletable   BOOLEAN      NOT NULL DEFAULT TRUE,
+    reading_status TEXT         NULL,
+    created_at     TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT chk_shelf_status CHECK (reading_status IN ('TO_READ', 'READING', 'FINISHED')),
+    UNIQUE (user_id, name)
 );
-
-ALTER TABLE shelves
-    ADD COLUMN is_deletable BOOLEAN NOT NULL DEFAULT TRUE;
-
-ALTER TABLE shelves
-    ADD COLUMN reading_status TEXT NULL,
-    ADD CONSTRAINT chk_books_status
-        CHECK (reading_status IN ('TO_READ', 'READING', 'FINISHED'));
-
 
 CREATE TABLE IF NOT EXISTS books
 (
     id            SERIAL PRIMARY KEY,
+    user_id       INTEGER      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
+    shelf_id      INTEGER      NOT NULL REFERENCES shelves (id) ON DELETE CASCADE,
     google_id     VARCHAR(250) NOT NULL,
     title         TEXT         NOT NULL,
     authors       TEXT[]       NOT NULL,
     thumbnail_url TEXT,
     current_page  INTEGER      NULL,
     page_count    INTEGER      NULL,
+    rating        INTEGER      NULL,
+    review        TEXT         NULL,
     created_at    TIMESTAMPTZ  NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    user_id       INTEGER      NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    shelf_id      INTEGER      NOT NULL REFERENCES shelves (id) ON DELETE CASCADE
+    updated_at    TIMESTAMPTZ  NULL,
+    finished_at   TIMESTAMPTZ  NULL,
+    CONSTRAINT uq_books_user_google UNIQUE (user_id, google_id)
 );
-
-ALTER TABLE books
-    ADD CONSTRAINT uq_books_user_google UNIQUE (user_id, google_id);
-
-ALTER TABLE books
-    ADD COLUMN updated_at TIMESTAMPTZ NULL;
-
-ALTER TABLE books
-    ADD COLUMN finished_at TIMESTAMPTZ NULL;
-
-ALTER TABLE books
-    ADD COLUMN rating INTEGER NULL;
-
-ALTER TABLE books
-    ADD COLUMN review TEXT NULL;
+CREATE INDEX idx_books_user_shelf ON books (user_id, shelf_id);
